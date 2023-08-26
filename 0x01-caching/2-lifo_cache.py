f9adc2370d4cdca586bdfa533006aa2693b17545
lloyd kioko <ch.abene254@gmail.com>
2023-08-27 01:18:41 +0300

    2nd commit

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,59 +1,37 @@
 #!/usr/bin/env python3
-
 """
-LIFOCache module
-Requirements:
-You must use self.cache_data - dictionary from
-the parent class BaseCaching
-You can overload def __init__(self): but don’t
-forget to call the parent init: super().__init__()
- def put(self, key, item):
-Must assign to the dictionary self.cache_data the
-item value for the key key.
-If key or item is None, this method should not do
-anything.
-If the number of items in self.cache_data is higher
-that BaseCaching.MAX_ITEMS:
-you must discard the last item put in cache
-(LIFO algorithm)
-you must print DISCARD: with the key discarded and
-following by a new line
- def get(self, key):
-Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn’t exist in
-self.cache_data, return None.
+LIFOCache class implementation
 """
-
-
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    # caching system that uses LIFO eviction policy
+    """
+    Defines a caching system class that implements LIFO algorithm
+    """
     def __init__(self):
-        # initialization
-
+        """
+        Initializes the class
+        """
         super().__init__()
 
     def put(self, key, item):
-        # assign item value to key
+        """
+        Assigns to the dictionary self.cache_data the item value for the key,
+        checking that BaseCaching.MAX_ITEMS isn't exceeded. Else, discard first
+        item following LIFO algorithm
+        """
+        if key and item:
+            self.cache_data.update({key: item})
 
-        if key is None or item is None:
-            pass
-        else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                    and key not in self.cache_data.keys():
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                self.cache_data.pop(self.last_inserted)
+                print("DISCARD: {}".format(self.last_inserted))
 
-                # delete the last item in the dictionary
-                last_key, last_value = self.cache_data.popitem()
-
-                print("DISCARD: {}". format(last_key))
-
-            self.cache_data[key] = item
+            self.last_inserted = key
 
     def get(self, key):
-        # return the value in self.cache_data linked to key
-
-        if key is None or key not in self.cache_data.keys():
-            return None
+        """
+        Returns the value in self.cache_data linked to key
+        """
         return self.cache_data.get(key)
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,61 +1,50 @@
 #!/usr/bin/env python3
-
-"""Create a class LRUCache that inherits from
-BaseCaching and is a caching system:
-
-You must use self.cache_data - dictionary from
-the parent class BaseCaching
-You can overload def __init__(self): but don’t
-forget to call the parent init: super().__init__()
- def put(self, key, item):
-Must assign to the dictionary self.cache_data the
-item value for the key key.
-If key or item is None, this method should not do
-anything.
-If the number of items in self.cache_data is higher
-that BaseCaching.MAX_ITEMS:
-you must discard the least recently used item
-(LRU algorithm)
-you must print DISCARD: with the key discarded
-and following by a new line
- def get(self, key):
-Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn’t exist in
-self.cache_data, return None.
 """
-
-
-BaseCaching = __import__('base_caching').BaseCaching
+LRUCache class implementation
+"""
+from base_caching import BaseCaching
+from collections import deque
 
 
 class LRUCache(BaseCaching):
-    # caching system
+    """
+    Defines a caching system class that implements LRU algorithm
+    """
+    # Queue of items accessed in dictionary using get
+    KEYS_ACCESSED = deque([])
 
     def __init__(self):
-        # initialization
-
+        """
+        Initializes the class
+        """
         super().__init__()
-        self.usedKeys = []
 
     def put(self, key, item):
-        # assign item value for the key
+        """
+        Assigns to the dictionary self.cache_data the item value for the key,
+        checking that BaseCaching.MAX_ITEMS isn't exceeded. Else, discard first
+        item following LRU algorithm
+        """
+        if key and item:
+            self.cache_data.update({key: item})
 
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            if key not in self.usedKeys:
-                self.usedKeys.append(key)
+            if key in LRUCache.KEYS_ACCESSED:
+                LRUCache.KEYS_ACCESSED.remove(key)
+                LRUCache.KEYS_ACCESSED.append(key)
             else:
-                self.usedKeys.append(
-                    self.usedKeys.pop(self.usedKeys.index(key)))
-            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(0)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+                LRUCache.KEYS_ACCESSED.append(key)
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                least_used_key = LRUCache.KEYS_ACCESSED.popleft()
+                self.cache_data.pop(least_used_key)
+                print("DISCARD: {}".format(least_used_key))
 
     def get(self, key):
-        # return the value in self.cache_data linked to key
+        """
+        Returns the value in self.cache_data linked to key
+        """
+        if key in LRUCache.KEYS_ACCESSED:
+            LRUCache.KEYS_ACCESSED.remove(key)
+            LRUCache.KEYS_ACCESSED.append(key)
 
-        if key is not None and key in self.cache_data.keys():
-            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
-            return self.cache_data.get(key)
-        return None
+        return self.cache_data.get(key)
